4f4195ca098d73b959d75d94b6d96020723efb4a
17043003 <ishizuka533725@gmail.com>
2021-07-14 00:15:21 +0900

    install @types/react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { BrowserRouter, Route } from "react-router-dom";
 
 import "./App.css";
 import { endPoint } from "./Config";
